eb2f242d977332e133a1f8d5ab08fbfb09310094
Jannie <jz3191@columbia.edu>
2020-05-24 16:38:25 -0700

    testing making a server with node.js

--- test.js
+++ test.js
@@ -1,43 +1,14 @@
-let http = require('http');
-let fs = require('fs');
- 
-let handleRequest = (request, response) => {
-    response.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    fs.readFile('./index.html', null, function (error, data) {
-        if (error) {
-            response.writeHead(404);
-            respone.write('Whoops! File not found!');
-        } else {
-            response.write(data);
-        }
-        response.end();
-    });
-};
- 
-http.createServer(handleRequest).listen(8000);
-
-/*var http = require("http");
+var http = require("http");
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: true });
  
-app.set('view engine', 'html')
-//app.get('/',(req,res))
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
-}).listen(8080);
-
-
 // Running Server Details.
-var server = app.listen(8080, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log("Example app listening at %s:%s Port", host, port);
+var server = app.listen(8082, function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log("Example app listening at %s:%s Port", host, port)
 });
  
  
@@ -63,4 +34,4 @@ app.post('/thank', urlencodedParser, function (req, res){
   reply += "Your address is" + req.body.address;
   reply += "Your mobile number is" + req.body.mobilno;
   res.send(reply);
- });*/
+ });
