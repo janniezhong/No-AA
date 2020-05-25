03c24d962e214bdd383ec99a0f3bd763b54b655c
Jannie <jz3191@columbia.edu>
2020-05-25 01:16:54 -0700

    looks a little better now, and should work!

--- test.js
+++ test.js
@@ -1,27 +1,49 @@
 var http = require("http");
 var express = require('express');
+var request = require('request');
 var app = express();
 var bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({ extended: true });
- 
-var options = {
-    host: 'lcmspubcontact.lc.ca.gov',
-    path: '/PublicLCMS/ContactPopupSubmit.php',
-    port: '1338',
-    method: 'POST',
-    headers: {'custom': 'Custom Header Demo works'}
+var path = require('path');
+//var urlencodedParser = bodyParser.urlencoded({ extended: true });
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(express.static('public')); /* this line tells Express to use the public folder as our static folder from which we can serve static files*/
+
+
+var postData = {
+  district : 'AD',
+  inframe: 'N',
+  headerimg: 'Y',
+  txtFirstName : '',
+  txtLastName : '',
+  txtAddress: '',
+  txtCity: '',
+  txtZip: '',
+  txtAreaCode: '',
+  txtPhone1: '',
+  txtPhone2: '',
+  txtExt: '',
+  phoneTypeListbox: 'home',
+  txtEmail: '',
+  IssueListBox: '',
+  text_num:'',
+  jv_text:'',
+  submitButton: '',
+
 };
 
-callback = function(response) {
-    var str = ''
-    response.on('data', function (chunk) {
-      str += chunk;
-    });
-  
-    response.on('end', function () {
-      console.log(str);
-    });
-}
+var url = 'https://lcmspubcontact.lc.ca.gov/PublicLCMS/ContactPopupSubmit.php'
+
+var options = {
+    /*host: 'lcmspubcontact.lc.ca.gov',
+    connection: 'keep-alive',
+    origin: 'https://lcmspubcontact.lc.ca.gov',
+    path: '/PublicLCMS/ContactPopupSubmit.php', */
+    method: 'post',
+    body: postData,
+    json: true,
+    url: url,
+
+};
 
 
 
@@ -31,34 +53,52 @@ var server = app.listen(8080, function () {
     console.log("Example app listening at %s:%s Port", host, port);
 });
 
-app.get('/form', function (req, res) {
-  var html='';
-  html +="<body>";
-  html += "<form action='/thank'  method='post' name='form1'>";
-  html += "Name:<input type= 'text' name='name'></p>";
-  html += "Email:<input type='text' name='email'></p>";
-  html += "address:<input type='text' name='address'></p>";
-  html += "Mobile number:<input type='text' name='mobilno'></p>";
-  html += "<input type='submit' value='submit'>";
-  html += "<INPUT type='reset'  value='reset'>";
-  html += "</form>";
-  html += "</body>";
-  res.send(html);
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname+'/public/index.html'));
 });
  
-app.post('/thank', urlencodedParser, function (req, res){
-  var reply='';
-  reply += "Your name is" + req.body.name;
-  reply += "Your E-mail id is" + req.body.email; 
-  reply += "Your address is" + req.body.address;
-  reply += "Your mobile number is" + req.body.mobilno;
-  res.send(reply);
+app.post('/thank', function (req, res){
+  res.sendFile(path.join(__dirname+'/public/thank.html'));
+
+  postData.txtFirstName = req.body.txtFirstName;
+  postData.txtLastName = req.body.txtLastName;
+  postData.txtAddress = req.body.txtAddress;
+  postData.txtCity = req.body.txtCity;
+  postData.txtZip = req.body.txtZip;
+  postData.txtAreaCode = req.body.txtAreaCode;
+  postData.txtPhone1 = req.body.txtPhone1;
+  postData.txtPhone2 = req.body.txtPhone2;
+  postData.txtExt = req.body.txtExt;
+  postData.txtEmail = req.body.txtEmail;
+  postData.jv_txt = req.body.jv_text;
+  postData.txtNum = 2000 - postData.jv_txt.length;
+
+  for (let i = 1; i < 31; i++){
+  postData.district += i;
+
+  request(options, 
+  function (err, httpResponse, body) {
+    if (err) {
+      console.error('error posting json: ', err)
+      throw err
+    }
+    var headers = httpResponse.headers
+    var statusCode = httpResponse.statusCode
+    console.log('headers: ', headers)
+    console.log('statusCode: ', statusCode)
+    console.log('body: ', body)
+
+  })
+  
+  postData.district = "AD";
+
+  }
 
 
-
+  /*
   var req = http.request(options, callback);
   req.end();
-
+  */
  });
 
 
