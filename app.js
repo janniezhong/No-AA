4aef558b2ac498c49614b9d433c251a2c7d07287
Jannie <jz3191@columbia.edu>
2020-08-09 20:20:31 -0700

    mass update - changed a lot of things, project now complete!

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ var request = require('request');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
+const storage = require('node-persist');
+const csv = require('csv-parser');
+const fs = require('fs');
+var HashMap = require('hashmap');
+
+
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
@@ -14,7 +20,41 @@ app.use('/', express.static('views')); /* this line tells Express to use the pub
 //const clientFilesPath = path.join(__dirname, "./data-files/")
 //const fileHandler = new StaticFileHandler(clientFilesPath);
 
+var zipmap = new HashMap();
 
+
+fs.createReadStream('views/data.csv')
+  .pipe(csv())
+  .on('data', function (row) {
+
+    //console.log(row.ZipCodes);
+    var zipString = row.ZipCodes.toString();
+    var zipArray = zipString.split(",");
+    var dist = row.AD.toString();
+
+    for (var j = 0; j<zipArray.length; j++){
+      if (zipmap.has(zipArray[j])){
+        var curr = zipmap.get(zipArray[j]).push(dist);
+      } else {
+        zipmap.set(zipArray[j],[dist]);
+      }
+    }
+
+    //console.log(zipmap.size);
+
+
+  })
+  .on('end', () => {
+    //console.log(zipmap.get("94087"));
+
+  });
+
+
+var counter = 0;
+var districts = ["AD17", "AD77", "AD66", "AD63", "AD27", "AD49", "AD25", "AD19", "AD02", "AD08", "AD38", "AD28", "AD76", "AD74", "AD20", "AD18", "AD65", "AD16", "SD34", "SD05", "SD10", "SD06", "SD31", "SD22", "SD09", "SD11", "SD15", "SD18", "SD40", "SD32", "SD03"];
+
+var districts = ["AD01","AD05","AD13","AD16","AD18","AD20","AD30","AD34","AD45","AD49","AD50","AD51","AD56","AD57","AD72","AD73","AD74","AD80","SD01","SD02","SD03","SD04","SD05","SD06","SD07","SD08","SD09","SD10","SD11","SD12","SD13","SD14","SD15","SD16","SD17","SD18","SD19","SD20","SD21","SD22","SD23","SD24","SD25","SD26","SD27","SD28","SD29","SD30","SD31","SD32","SD33","SD34","SD35","SD36","SD37","SD38","SD39","SD40"];
+//, "SD34", "SD05", "SD10", "SD06", "SD31", "SD22", "SD09", "SD11", "SD15", "SD18", "SD40", "SD32", "SD03"
 
 var postData = {
   district: 'AD',
@@ -34,7 +74,7 @@ var postData = {
   IssueListBox: '',
   text_num: '',
   jv_text: '',
-  submitButton: '',
+  submitButton: 'Submit',
 
 };
 
@@ -45,18 +85,41 @@ var options = {
   connection: 'keep-alive',
   origin: 'https://lcmspubcontact.lc.ca.gov',
   path: '/PublicLCMS/ContactPopupSubmit.php', */
+  headers: {
+    //'Content-Type': 'application/x-www-form-urlencoded',
+    'Host': 'lcmspubcontact.lc.ca.gov',
+    'Connection': 'keep-alive',
+    'Origin': 'https://lcmspubcontact.lc.ca.gov',
+    'Referer': 'https://lcmspubcontact.lc.ca.gov/PublicLCMS/ContactPopup.php?district=',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-User': '?1',
+    'Sec-Fetch-Dest': 'document',
+  },
   method: 'post',
   body: postData,
   json: true,
   url: url,
+  'Path': '/PublicLCMS/ContactPopupSubmit.php',
 
 };
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+
+storage.init().then(() => storage.getItem("counter")).then((value) => {
+  // Checks if value read is valid, otherwise set it to 0
+  if (value > 35000) {
+      counter = value;
+  } else {
+      counter = 34897;
+  }
+  console.log(counter);
+  // Start the server
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
   console.log('Press Ctrl+C to quit.');
+});
+
 });
 
 // [END gae_flex_node_static_files]
@@ -66,16 +129,36 @@ module.exports = app;
 
 
 app.get('/', function (req, res) {
-  res.render('index.html');
+  
+  res.json(counter);
+  console.log("Counter is currently "+ counter+".");
+  //res.render('index.html');
+
+  res.render("index.html", {counter:counter});
+  
+  
 });
 app.get('/index.html', function (req, res) {
-  res.render('index.html');
+  
+  //res.json(counter);
+  res.render("index.html", {counter:counter});  
 });
 
+app.get('/counter', function (req, res) {
+  res.json(counter);
+});
+
+
 app.post('/thank', function (req, res) {
-  console.log("here!");
+
   res.render('thank.html');
-  console.log("here 2.0!");
+  counter++;    
+  //console.log(counter + ":2nd one");
+
+  
+  // Saves counter into the store and send response AFTER the store has been saved
+  storage.setItem("counter", counter);
+
 
   postData.txtFirstName = req.body.txtFirstName;
   postData.txtLastName = req.body.txtLastName;
@@ -90,8 +173,11 @@ app.post('/thank', function (req, res) {
   postData.jv_txt = req.body.jv_text;
   postData.txtNum = 2000 - postData.jv_txt.length;
 
-  for (let i = 1; i < 16; i++) {
-    postData.district += i;
+
+  for (var i of districts) {
+    //setTimeout(wait, 3000);
+    postData.district = i;
+    options.headers.Referer += postData.district;
 
     request(options,
       function (err, httpResponse, body) {
@@ -107,7 +193,26 @@ app.post('/thank', function (req, res) {
 
       })
 
-    postData.district = "AD";
+    //postData.district = "AD";
+    options.headers.Referer = 'https://lcmspubcontact.lc.ca.gov/PublicLCMS/ContactPopup.php?district=';
+
+  }
+
+  for (var i of zipmap.get(postData.txtZip)){
+    postData.district = i;
+    request(options,
+      function (err, httpResponse, body) {
+        if (err) {
+          console.error('error posting json: ', err)
+          throw err
+        }
+        var headers = httpResponse.headers
+        var statusCode = httpResponse.statusCode
+        console.log('headers: ', headers)
+        console.log('statusCode: ', statusCode)
+        console.log('body: ', body)
+
+      })
 
   }
 
@@ -127,5 +232,14 @@ app.post('/thank', function (req, res) {
 
 
 //module.exports.handler = app;
+
+function wait() {
+  // all the stuff you want to happen after that pause
+  //await delay();
+  console.log('waiting');
+}
+function delay() {
+  return new Promise(resolve => setTimeout(resolve, 300));
+}
 
 
